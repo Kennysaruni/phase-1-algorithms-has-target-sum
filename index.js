e3b40d489e7d9006c05dcf0c67c57c34adc7310e
Kennysaruni <kenny.saruni@student.moringaschool.com>
2022-12-11 11:00:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
-
+ // Write your algorithm here
+  function hasTargetSum(array, target) {
+    for (let i = 0; i < array.length; i++) {
+      // n steps (depending on the length of the input array)
+      const complement = target - array[i];
+      for (let j = i + 1; j < array.length; j++) {
+        // n * n steps (nested loop!)
+        if (array[j] === complement) return true;
+      }
+    }
+    // 1 step
+    return false;
+  }
 /* 
   Write the Big O time complexity of your function here
 */
